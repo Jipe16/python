bb2ea8eb0e4088592255b1a1bf82eadab3263676
Jipe <109040883+Jipe16@users.noreply.github.com>
2023-05-23 10:15:24 -0300

    Ex002, 003 adicionados

---
+++ ex002.py
@@ -0,0 +1,5 @@
+pn = int(input('Primeiro núnero: '))
+sn = int(input('Segundo número: '))
+s = pn + sn
+#print('A soma entre ',pn ,'e', sn, 'é igual a : {}'.format(s))
+print('A some entre {} e {} vale {}'.format(pn,sn,s))
---
+++ ex003.py
@@ -0,0 +1,4 @@
+algo = input('Digite algo: ')
+print('É alphanumérico?',algo.isalnum())
+print('É tudo minusculo?',algo.islower())
+print('É tudo maiusculo?',algo.isupper())
