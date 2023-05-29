65593bc83280ab02a3a8f56714cafe9f9eb07253
Jipe <109040883+Jipe16@users.noreply.github.com>
2023-05-29 13:55:14 -0300

    Aula 07 criada

---
+++ aula07.py
@@ -0,0 +1,14 @@
+# n1 = int(input('Digite um valor: '))
+# n2 = int(input('Digite outro valor: '))
+# s = n1 + n2
+# m = n1 * n2
+# d = n1 / n2
+# di = n1 // n2
+# e = n1 ** n2
+# print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(s,m,d), '\nDivisão inteira {} e potência {}'.format(di,e))
+
+# n1 = int(input('Digite um número: '))
+# suc = n1 + 1
+# ant = n1 - 1
+# print('O sucessor e antecessor do número {} são, respectivamente : {} e {}'.format(n1,suc,ant))
+
