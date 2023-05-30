c38ff7fdeaf4285caa94ae4c21f99b390ff1624b
Jipe <109040883+Jipe16@users.noreply.github.com>
2023-05-30 16:02:51 -0300

    Aula 07 concluída e 08 iniciada

--- aula07.py
+++ aula07.py
@@ -1,3 +1,5 @@
+#EXERCICIO 4
+
 # n1 = int(input('Digite um valor: '))
 # n2 = int(input('Digite outro valor: '))
 # s = n1 + n2
@@ -7,8 +9,85 @@
 # e = n1 ** n2
 # print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(s,m,d), '\nDivisão inteira {} e potência {}'.format(di,e))
 
+#======================================================================================================
+#EXERCICIO 5
+
 # n1 = int(input('Digite um número: '))
 # suc = n1 + 1
 # ant = n1 - 1
 # print('O sucessor e antecessor do número {} são, respectivamente : {} e {}'.format(n1,suc,ant))
 
+#=======================================================================================================
+#EXERCICIO 6
+
+# n1 = int(input('Digite um número: '))
+# db = n1 * 2
+# tp = n1 * 3
+# r = n1 ** (1/2)
+# print('O dobro de {} é {}, o triplo é {} e a raiz é {} !'.format(n1,db,tp,r))
+
+#========================================================================
+#EXERCICIO 7
+
+# n1 = float(input('Digite a primeira nota: '))
+# n2 = float(input('Digite a segunda nota: '))
+# m = (n1 + n2)/2
+# print('A primeira nota foi {} \nA segunda foi {} \nA média é igual a {}'.format(n1,n2,m))
+
+#======================================================================
+#EXERCICIO 8
+
+# n1 = float(input('Digite uma quantidade em metros: '))
+# cm = n1 * 100
+# mm = n1 * 1000
+# print('{:.2f} metros é igual a :\n{:.2f} centímetros \n{:.2f} milímetros.'.format(n1,cm,mm))
+
+#===========================================================================
+#EXERCICIO 9
+
+# n = int(input("Digite um número para ver a tabuada: "))
+# for i in range(1, 11):
+#     resultado = n * i
+#     print(n, "x", i, "=", resultado)
+
+#============================================================================
+#EXERCICIO 10
+
+# r = float(input('Quantos reais você possui? '))
+# d = r / 3.27
+# print('Você pode comprar {:.2f} dólares.'.format(d))
+#==============================================================
+#EXERCICIO 11
+
+# l = float(input('Digite a largura(em metros): '))
+# a = float(input('Digite a altura(em metros): '))
+# ar = l * a
+# ti = ar / 2
+# print('A área que precisa ser pintada é {} m² e sera necessário {} litros de tinta'.format(ar, ti))
+#==========================================================================
+#EXERCICIO 12
+
+# p = float(input('Digite o preço do produto: '))
+# desc = p * 0.95
+# print('O preço com desconto é igual a R${:.2f}!'.format(desc))
+#==========================================================================
+#EXERCICIO 13
+
+# s = float(input('Digite o salário do funcionário: '))
+# r = s * 1.15
+# print('O novo salário com reajuste será de R${:.2f}!'.format(r))
+#===================================================
+#EXERCICIO 14
+
+# c = float(input('Digite a temperatura em Cº: '))
+# f = c * (9/5) + 32
+# k = c + 273.15
+# print('A temperatura {}ºC é igual a {}F e {}K'.format(c,f,k))
+# ================================================================
+# EXERCICIO 15
+
+# dias = int(input('Quantos dias alugados? '))
+# km = float(input('Quantos KM rodados? '))
+# pagar = (dias * 60) + (km * 0.15)
+
+# print('O total a pagar é ',pagar)
---
+++ aula08.py
@@ -0,0 +1,18 @@
+#EXERCICIO 16
+# n = float(input('Digite um número real: '))
+# i = int(n)
+# print('A parte inteira do número {} é: {}'.format(n,i))
+
+# from math import trunc
+# #EXERCICIO 16
+# n = float(input('Digite um número real: '))
+# print('A parte inteira do número {} é: {}'.format(n,trunc(n)))
+#====================================================
+#EXERCICIO 17
+# from math import hypot
+# co = float(input('Comprimento do cateto oposto: '))
+# ca = float(input('Comprimento do cateto adjacente: '))
+# hi = hypot(co,ca)  ou hi = (co ** 2 + ca) ** 2 (1/2)
+# print('A hipotenusa vai medir {:.2f}'.format(hi))
+#=====================================================
+#EXERCICIO 18
